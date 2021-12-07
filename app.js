5b2b3a9552e10e5c220bb738c788a48130166ed9
Sanjeev Sharma <sanjeevsharma1603@gmail.com>
2021-12-07 21:42:27 +0530

    final code submitted

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const socket = require("socket.io");
+
+const app = express(); // initialize and server ready
+app.use(express.static("public")); //ye kya karega ki ye public folder mein jake index.html folder ko khol dega
+const port = 5000;
+//process.env.PORT ||
+let server = app.listen(port, () => {
+  console.log("Listening to port " + port); // koi request karega to yahan request listen hogi
+});
+let io = socket(server); //initialization
+io.on("connection", (socket) => {
+  console.log("Made socket connection");
+  // Received data
+  socket.on("beginPath", (data) => {
+    // data -> data from frontend
+    // Now transfer data to all connected computers
+    io.sockets.emit("beginPath", data);
+  });
+  socket.on("drawStroke", (data) => {
+    io.sockets.emit("drawStroke", data);
+  });
+  socket.on("redoUndo", (data) => {
+    io.sockets.emit("redoUndo", data);
+  });
+});
+//"start": "node app.js"
